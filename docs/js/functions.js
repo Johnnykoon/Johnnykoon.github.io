71db6fb5699ab3852f2a3fdbe0d1aa62e7e9665c
Johnnykoon <121228634+Johnnykoon@users.noreply.github.com>
2024-03-07 11:48:03 +0000

    Add files via upload

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -62,6 +62,12 @@ Amplitude.init({
 	"songs": [
 		
 		{
+			"name": "do a 180",
+			"album": "英文單字強",
+			"url": "sub/audio/170.mp3",
+			"cover_art_url": "sub/image/music-streaming.png"
+		},
+		{
 			"name": "take shape",
 			"album": "英文單字強",
 			"url": "sub/audio/169.mp3",
